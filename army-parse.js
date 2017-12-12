34af2b322c13abc8420267d9f5f2ddbf1582c1cd
lawrie-sm <alertrelic@gmail.com>
2017-12-12 14:49:00 +0000

    Added unit and min/max ranges. Parser largely complete

--- army-parse.js
+++ army-parse.js
@@ -13,13 +13,14 @@ fs.readFile(filename, 'utf8', parseArmyList);
 let isFirstChunk = true;
 let chunkName = '';
 let currChunk = {};
+let currUnitI = -1;
 let output = [];
 
 function parseArmyList(err, data) {
   if (err) throw err;
   let lines = data.split('\r\n');
   lines.forEach(parseLine);
-  console.log(output);
+  console.log(output[19]);
 }
 
 function parseLine(line, lineNo, lines) {
@@ -43,7 +44,7 @@ function parseLine(line, lineNo, lines) {
 
         let captureTagName = /\[(.*)\]/g;
         let chunkName = captureTagName.exec(line)[1];
-        output.push({identifier: chunkName});
+        output.push({identifier: chunkName, units: []});
         currChunk = output[output.length - 1];
         
         return (0);
@@ -53,14 +54,62 @@ function parseLine(line, lineNo, lines) {
         return (0);
       }
     } else if (currChunk) {
-
       let words = line.split(' ');
-      if (words[0] === 'SORTNAME') {
-        currChunk.name = words[1];
-      }
-
+      parseWords (words);
     }
 
   }
   
+}
+
+function parseWords (words) {
+  
+  if (words[0] === 'SORTNAME') {
+
+    currChunk.name = words[1];
+
+  } else if (words[0] === 'YEARS') {
+
+    /*
+    Must subtract 3000 as the game start date is 3000BC
+    a negative number is BC, a positive one is AD.
+    */
+
+    currChunk.startDate = parseInt(words[1]) - 3000;
+    currChunk.endDate = parseInt(words[2]) - 3000;
+
+  } else if (words[0] === 'DEPLOYMENT') {
+
+    currChunk.demploymentStyle = words[1];
+
+  } else if (words[0] === 'MAPSET') {
+
+    currChunk.map = words[1];
+
+  } else {
+
+    let captureUnitID = /UNIT_([0-9])/g;
+    let unitMatch = captureUnitID.exec(words[0]);
+    if (unitMatch) {
+      currUnitI = parseInt(unitMatch[1]);
+      currChunk.units[currUnitI] = {
+        name: words[1],
+        min: -1,
+        max: -1 };
+      return (0);
+    }
+
+    let captureUnitRange = /(MIN|MAX)_([0-9])/g;
+    let rangeMatch = captureUnitRange.exec(words[0]);
+    if (rangeMatch) {
+      let value = parseInt(words[1]);
+      let unitI = parseInt(rangeMatch[2]);
+      let rangeName = rangeMatch[1].toLowerCase();
+      currChunk.units[unitI][rangeName] = value;
+      return (0);
+    }
+
+  }
+
+
 }
